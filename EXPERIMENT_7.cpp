020a77c09233135681a125e9c85c06c259256c73
yashiscoding100 <157284659+yashiscoding100@users.noreply.github.com>
2024-08-13 15:42:23 +0530

    Update EXPERIMENT_7.cpp

--- EXPERIMENT_7.cpp
+++ EXPERIMENT_7.cpp
@@ -23,8 +23,7 @@ T findMax(T a, T b, T c) {
 int main() {
    
     int a = 5, b = 9, c = 3;
-    std::cout << "The largest integer is: " << findMax(a, b, c) << std::endl;
-
+   std::cout << "The largest integer is: " << findMax(a,b,c) << std::endl;
     double x = 5.7, y = 9.1, z = 3.4;
     std::cout << "The largest double is: " << findMax(x, y, z) << std::endl;
 
